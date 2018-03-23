93403582fd85ff6ddaebd1c6899f1f6ecea02c08
mysha13 <33964357+mysha13@users.noreply.github.com>
2018-03-23 22:21:12 +0100

    windows are done
    windows and class current info

---
+++ boxitem/boxitem/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace boxitem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Register r = new Register();
+            //r.Show();
+            //Boxes b = new Boxes();
+            //b.Show();
+            //Items i = new Items();
+            //i.Show();
+            //RemindPassword rp = new RemindPassword();
+            //rp.Show();
+            //FindItem fi = new FindItem();
+            //fi.Show();
+            //BoxesList bl = new BoxesList();
+            //bl.Show();
+            AddPhoto ap = new AddPhoto();
+            ap.Show();
+            AddItem ai = new AddItem();
+            ai.Show();
+        }
+    }
+}
